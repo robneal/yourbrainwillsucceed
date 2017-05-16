1351d4678e5027cacc0faa636a48b91c774b4b99
robneal <robneal28@gmail.com>
2017-05-16 13:21:41 -0400

    Updated video and js files

--- README.md
+++ README.md
@@ -4,10 +4,8 @@ Author(s): Robert Neal
 
 Description: FrontEnd Project (Static Files Only) Developed for Dummont Innovative Technologies under contract with Sha Digital marketing. 
 
-Project Status : In Deployment
+Project Status : Completed
 
 GitHub Pages Link - https://robneal.github.io/yourbrainwillsucceed/
 
-Digtal Ocean Droplet IP Addreass - http://104.236.52.227/
-
-Website Link - http://www.yourbrainwillsucceed.com/
+Website Link - http://www.dumontocdhelp.com/
--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+jQuery(document).ready(function($){
 	
 	console.log("js is (t)werking");
-
-	$('.nojs').hide(); 
-	// To solve my with problem I can add the class large at width 960px and run all my js on .large 
+	$('.nojs').hide();  // To solve my with problem I can add the class large at width 960px and run all my js on .large 
 	
+	
+
 	// Smooth Scroolling on all local anchor links 
 	$(function() {
 	  $('a[href*="#"]:not([href="#"])').click(function() {
@@ -22,8 +22,109 @@ $(document).ready(function(){
 	});  
 
 
-	var end = new Date('06/28/2017 10:1 AM');
 
+	// SCROLLING EFFECTS FOR STORY PAGE
+	if( document.getElementById('story') != null ){
+	  	
+	  	$('#story .container .fadeInSection').addClass('fadeOut-right');
+
+		// Parallax Effect for when user scrolls page vertically 
+	    $(window).scroll(function(){
+			
+			// Delare variables
+			var pixScrolled = $(this).scrollTop(); 
+
+			var $visualText = $("#visualLead .container"); 
+			var $scaleBy = pixScrolled / 10100 +1 ;
+			var $storyContainer =$("#story .container"); 
+			// Debuging 
+			//console.log(pixScrolled); 
+			//console.log("ScaleFactor: " + $scaleBy);
+			
+
+			// Visual Lead
+			
+			// $('#visualLead').css({transform: 'scale('+ $scaleBy +','+ $scaleBy+')'}); // Scale leading image
+			
+			$visualText.css({
+				'transform' : 'translate(-50%, ' + (-50 + pixScrolled /3.5) + '%)'
+			});
+			
+			if(pixScrolled > 320){
+				$visualText.fadeOut(800);
+			}else{
+				$visualText.fadeIn(800);
+			}
+
+
+			// Story Section - Hope Section fade In/Fade Out 
+			if(pixScrolled > $('#story').offset().top - 60 ){
+				//console.log("hope"); 
+			    $('#story .container .hope').addClass('fadeIn-left');
+	  		}else{
+	  			$('.hope').removeClass('fadeIn-left');
+	  		}
+
+	  		// Story Section - Guidance Section ade In/Fade Out
+			if(pixScrolled > $('.hope').offset().top - 60 ){ 
+				//console.log("guidance"); 
+			    $('#story .container .guidance').addClass('fadeIn-left');
+	  		}else{
+	  			$('.guidance').removeClass('fadeIn-left');
+	  		}
+
+	  		// Story Section - Community Section fade In/Fade Out
+	  		if(pixScrolled > $('.guidance').offset().top - 60 ){
+				//console.log("Community"); 
+			    $('#story .container .community').addClass('fadeIn-left');
+	  		}else{
+	  			$('.community').removeClass('fadeIn-left');
+	  		}
+
+
+	  		//	Main Menu Nav Links Styling
+	  		
+	  		var $mainMenuLinks = $('.mainMenu a');
+	  		
+	  		$mainMenuLinks.click( function(){
+	  			console.log ('Clicked a'); 
+	  			$('#hbMenu').prop('checked', false);
+	  		}); 	
+
+	  		//var $homeLink = $('.mainMenu a:nth-child(1)');
+	  		var $storyLink = $('.mainMenu a:nth-child(1)');
+	  		var $teamLink = $('.mainMenu a:nth-child(2)');
+	  		var $contactLink = $('.mainMenu a:nth-child(3)');
+	  		
+	  		
+			if ((pixScrolled > $('#story').offset().top - 60) && (pixScrolled <= $('#team').offset().top - 60)){
+	    		//$homeLink.removeClass('menuBottom');
+	    		$teamLink.removeClass('menuBottom');
+				$contactLink.removeClass('menuBottom');
+
+	    		$storyLink.addClass('menuBottom');
+
+			}else if((pixScrolled > $('#team').offset().top - 60) && (pixScrolled <= $('#contact').offset().top - 60)){
+				//$homeLink.removeClass('menuBottom');
+				$storyLink.removeClass('menuBottom');
+				$contactLink.removeClass('menuBottom');
+
+				$teamLink.addClass('menuBottom');
+
+			}else if((pixScrolled > $('#team').offset().top - 60)){
+				//$homeLink.removeClass('menuBottom');
+				$storyLink.removeClass('menuBottom');
+				$teamLink.removeClass('menuBottom');
+
+				$contactLink.addClass('menuBottom');
+			}
+		});// window.scroll()
+	}
+
+
+
+	// COUNTDOWN FOR BOOK PAGE
+	var end = new Date('07/24/2017 8:00 AM');
     var _second = 1000;
     var _minute = _second * 60;
     var _hour = _minute * 60;
@@ -48,7 +149,7 @@ $(document).ready(function(){
         document.getElementById('countdown').innerHTML = 
         	'<li class="days countdownSection "><span id="dayCount" class="count">'
         	+ days + '</span><span class="title">Days</span></li>';
-    
+
         // document.querySelector('.countdown.days.count').innerHTML = days + 'days ';
        
         document.getElementById('countdown').innerHTML += 
@@ -63,201 +164,13 @@ $(document).ready(function(){
         document.getElementById('countdown').innerHTML += 
         	'<li class="seconds countdownSection "><span id="secsCount" class="count">'
         	+ seconds + '</span><span class="title">Seconds</span></li>';
-
+    }
+    
+    if( document.getElementById('bookStory') != null ){
+        timer = setInterval(showRemaining, 1000);
     }
 
-    timer = setInterval(showRemaining, 1000);
-
-
-
-
 	
-	// Typed JS typing text in visualLead 
-  //   $(".typingText").typed({
-  //   	strings: ["Dumont Innovative Technologies can help.","The road to OCD recovery starts here!" ],
-  //       typeSpeed: 75,
-        
-  //       // time before typing starts
-		// startDelay: 5000,
-		
-		// // backspacing speed
-		// backSpeed: 75,
-
-		// // time before backspacing
-		// backDelay: 2000,
-		
-		// // loop
-		// loop: true
-  //     });
-  //   $('.typed-cursor').addClass('hideMe');
-  	$('#story .container .fadeInSection').addClass('fadeOut-right');
-	// Parallax Effect for when user scrolls page vertically 
-    $(window).scroll(function(){
-		// Delare variables
-
-		
-		var pixScrolled = $(this).scrollTop(); 
-
-		var $visualText = $("#visualLead .container"); 
-		var $scaleBy = pixScrolled / 10100 +1 ;
-		var $storyContainer =$("#story .container"); 
-		// Debuging 
-		//console.log(pixScrolled); 
-		//console.log("ScaleFactor: " + $scaleBy);
-		
-
-		// Visual Lead
-		
-		// $('#visualLead').css({transform: 'scale('+ $scaleBy +','+ $scaleBy+')'}); // Scale leading image
-		
-		$visualText.css({
-			'transform' : 'translate(-50%, ' + (-50 + pixScrolled /3.5) + '%)'
-		});
-		
-		if(pixScrolled > 320){
-			$visualText.fadeOut(800);
-		}else{
-			$visualText.fadeIn(800);
-		}
-
-
-		// Story Section - Hope Section fade In/Fade Out 
-		if(pixScrolled > $('#story').offset().top - 60 ){
-			//console.log("hope"); 
-		    $('#story .container .hope').addClass('fadeIn-left');
-  		}else{
-  			$('.hope').removeClass('fadeIn-left');
-  		}
-
-  		// Story Section - Guidance Section ade In/Fade Out
-		if(pixScrolled > $('.hope').offset().top - 60 ){ 
-			//console.log("guidance"); 
-		    $('#story .container .guidance').addClass('fadeIn-left');
-  		}else{
-  			$('.guidance').removeClass('fadeIn-left');
-  		}
-
-  		// Story Section - Community Section fade In/Fade Out
-  		if(pixScrolled > $('.guidance').offset().top - 60 ){
-			//console.log("Community"); 
-		    $('#story .container .community').addClass('fadeIn-left');
-  		}else{
-  			$('.community').removeClass('fadeIn-left');
-  		}
-
-
-  		//	Main Menu Nav Links Styling
-  		
-  		var $mainMenuLinks = $('.mainMenu a');
-  		
-  		$mainMenuLinks.click( function(){
-  			console.log ('Clicked a'); 
-  			$('#hbMenu').prop('checked', false);
-  		}); 	
-
-  		var $homeLink = $('.mainMenu a:nth-child(1)');
-  		var $storyLink = $('.mainMenu a:nth-child(2)');
-  		var $teamLink = $('.mainMenu a:nth-child(3)');
-  		var $contactLink = $('.mainMenu a:nth-child(4)');
-  		
-  		if(pixScrolled  <= $('#story').offset().top - 60){
-			$storyLink.removeClass('menuBottom');
-			$teamLink.removeClass('menuBottom');
-			$contactLink.removeClass('menuBottom');
-
-			$homeLink.addClass('menuBottom');
-
-		}else if ((pixScrolled > $('#story').offset().top - 60) && (pixScrolled <= $('#team').offset().top - 60)){
-    		$homeLink.removeClass('menuBottom');
-    		$teamLink.removeClass('menuBottom');
-			$contactLink.removeClass('menuBottom');
-
-    		$storyLink.addClass('menuBottom');
-
-		}else if((pixScrolled > $('#team').offset().top - 60) && (pixScrolled <= $('#contact').offset().top - 60)){
-			$homeLink.removeClass('menuBottom');
-			$storyLink.removeClass('menuBottom');
-			$contactLink.removeClass('menuBottom');
-
-			$teamLink.addClass('menuBottom');
-
-		}else{
-			$homeLink.removeClass('menuBottom');
-			$storyLink.removeClass('menuBottom');
-			$teamLink.removeClass('menuBottom');
-
-			$contactLink.addClass('menuBottom');
-		}
-
-
-	});// window.scroll()
-
-	
-
-
-
-	// Display Hambuger Menu for screen sizes < 960px 
-	// console.log($( window ).width() + " : Starting Window Width");
-	// showHamMenu($( window ).width()); 
-
-	// // !BUG only checks every two pixels
-	// $( window ).resize(function() { 
-	//   // $( "body" ).prepend( "<div>" + $( window ).width() + "</div>" );
-	//   console.log($( window ).width());
-	//   showHamMenu($( window ).width());
-	// });
-
-	// function showHamMenu(width){
-		
-	// 	// 	Declare variables 
-	// 	var $mainMenu = 	$(".mainMenu");
-	// 	var $visualLead = 	$("#visualLead");
-	// 	var $headerNav = 	$('header');
-	// 	var $openHBMenu = 	$(".openHBMenu"); 
-	// 	var $closeHbmX = 	$(".closeHbmX"); 
-
-	// 	if(width < 580){
-	// 		// 	Steup hamburger menu if js enabled
-	// 		$mainMenu.toggleClass( "hideMe" ); // hide mainMenu elements
-	// 		$openHBMenu.toggleClass( "hideMe" ); // show hb menu
-			
-	// 		// Setup fixed nav if js is enabeled
-	// 		$headerNav.css({
-	// 			'position' : 'fixed'
-	// 		});
-	// 		$visualLead.css('margin-top', '60px');		
-
-
-	// 		// Toggle HB Menu when clicked
-	// 		$openHBMenu.click( function(){
-	// 			console.log("Hamburger Menu Clicked"); 
-	// 			$( this ).toggleClass( "hideMe" );
-	// 			$mainMenu.toggleClass( "hideMe" );
-	// 			$closeHbmX.toggleClass( "hideMe" );
-	// 		}); 
-
-	// 		// Toggle X HB Menu when clicked
-	// 		$closeHbmX.click( function(){
-	// 			console.log("Hamburger Menu X Clicked"); 
-	// 			$( this ).toggleClass( "hideMe" );
-	// 			$mainMenu.toggleClass( "hideMe" );
-	// 			$openHBMenu.toggleClass( "hideMe" );
-	// 		}); 
-		
-	// 	}else{
-			
-	// 		$mainMenu.removeClass( "hideMe" ); // show mainMenu elements
-	// 		$openHBMenu.addClass( "hideMe" ); // hide hb menu
-	// 		$closeHbmX.addClass("hideMe"); // hide hb x Menu
-
-	// 		// 
-	// 		// $headerNav.css({
-	// 		// 	'position' : 'static'
-	// 		// });
-
-	// 		// $visualLead.css('margin-top', '0');	 
-	// 	}
-	// }
 });
 
 
